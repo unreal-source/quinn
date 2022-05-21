60eb2999cbe690eb9653f41accdb75da1d0a2f36
Nick Pfisterer <nick@polymoon.net>
2022-05-20 17:52:28 -0700

    New listener: Member Leave

---
+++ src/listeners/member-leave.js
@@ -0,0 +1,18 @@
+import { Listener } from 'hiei.js'
+
+class MemberLeave extends Listener {
+  constructor () {
+    super({
+      name: 'MemberLeave',
+      emitter: 'client',
+      event: 'guildMemberRemove'
+    })
+  }
+
+  async run (member) {
+    const memberLog = await this.client.channels.fetch(process.env.MEMBER_LOG_CHANNEL)
+    return memberLog.send({ content: `:red_circle: <@${member.user.id}> left the community` })
+  }
+}
+
+export default MemberLeave
