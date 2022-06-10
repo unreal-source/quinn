f26bbeedd953ae461d75186fde5da954a3473e25
Nick Pfisterer <nick@polymoon.net>
2022-06-10 11:32:57 -0700

    :sparkles: New command: Unlock Channel

---
+++ src/commands/moderation/unlock.js
@@ -0,0 +1,59 @@
+import { SlashCommand } from 'hiei.js'
+import { ApplicationCommandOptionType, EmbedBuilder, PermissionsBitField } from 'discord.js'
+
+class UnlockChannel extends SlashCommand {
+  constructor () {
+    super({
+      name: 'unlock',
+      description: 'Restore chatting and reacting permissions in a text channel',
+      options: [
+        {
+          type: ApplicationCommandOptionType.Channel,
+          name: 'channel',
+          description: 'The channel you want to unlock',
+          required: true
+        },
+        {
+          type: ApplicationCommandOptionType.String,
+          name: 'reason',
+          description: 'The reason for unlocking this channel'
+        }
+      ]
+    })
+  }
+
+  async run (interaction) {
+    const channel = interaction.options.getChannel('channel')
+    const reason = interaction.options.getString('reason')
+    const overwrites = channel.permissionOverwrites
+    const flags = PermissionsBitField.Flags
+
+    // abort if channel is not visible to @everyone
+    if (overwrites.cache.get(interaction.guild.id).deny.has(flags.ViewChannel)) {
+      return interaction.reply({ content: `${channel} is not visible to regular members so there's no need to unlock it.`, ephemeral: true })
+    }
+
+    // abort if channel already restricts chat and reactions for @everyone
+    if (overwrites.cache.get(interaction.guild.id).allow.has([flags.SendMessages, flags.AddReactions])) {
+      return interaction.reply({ content: `${channel} is already unlocked.`, ephemeral: true })
+    }
+
+    await channel.permissionOverwrites.edit(interaction.guild.id, {
+      SendMessages: true,
+      AddReactions: true
+    })
+
+    const moderationLog = interaction.guild.channels.cache.get(process.env.MODERATION_LOG_CHANNEL)
+    const moderationLogEntry = new EmbedBuilder()
+      .setAuthor({ name: '🔓 Channel unlocked' })
+      .setDescription(`**Channel:** #${channel.name}\n**Reason:** ${reason ?? 'No reason given'}`)
+      .setFooter({ text: interaction.member.user.tag })
+      .setTimestamp()
+
+    await moderationLog.send({ embeds: [moderationLogEntry] })
+    await channel.send({ content: ':unlock: Channel unlocked by a moderator. You may resume chatting.' })
+    return interaction.reply({ content: `${channel} successfully unlocked`, ephemeral: true })
+  }
+}
+
+export default UnlockChannel
