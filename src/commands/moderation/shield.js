88624c326ca9dead0ca49dfb0a6f066578e6dbfb
Nick Pfisterer <nick@polymoon.net>
2022-07-14 22:00:40 -0700

    Shield command now uses subcommands

--- src/commands/moderation/shield.js
+++ src/commands/moderation/shield.js
@@ -7,54 +7,109 @@ class Shield extends SlashCommand {
   constructor () {
     super({
       name: 'shield',
-      description: 'Enable to prevent new members from joining the server',
+      description: 'Prevent new members from joining the server',
       options: [
         {
-          type: ApplicationCommandOptionType.Boolean,
-          name: 'toggle',
-          description: 'Turn the shield on or off',
-          required: true
+          type: ApplicationCommandOptionType.Subcommand,
+          name: 'on',
+          description: 'Prevent new members from joining the server',
+          options: [
+            {
+              type: ApplicationCommandOptionType.String,
+              name: 'reason',
+              description: 'The reason for enabling shield',
+              required: true
+            }
+          ]
         },
         {
-          type: ApplicationCommandOptionType.String,
-          name: 'reason',
-          description: 'The reason for enabling/disabling the shield',
-          required: true
+          type: ApplicationCommandOptionType.Subcommand,
+          name: 'off',
+          description: 'Allow new members to join the server again',
+          options: [
+            {
+              type: ApplicationCommandOptionType.String,
+              name: 'reason',
+              description: 'The reason for disabling shield',
+              required: true
+            }
+          ]
         }
       ]
     })
   }
 
   async run (interaction) {
-    const toggle = interaction.options.getBoolean('toggle')
-    const reason = interaction.options.getString('reason')
-    const state = {
-      true: 'Shield enabled. New members who join the server will be automatically kicked until the shield is disabled.',
-      false: 'Shield disabled. New members may now join the server again.'
-    }
-
+    const subcommand = interaction.options.getSubcommand()
     const prisma = new PrismaClient()
-    await prisma.settings.upsert({
-      where: { guild: interaction.guild.id },
-      update: { shield: toggle },
-      create: {
-        guild: interaction.guild.id,
-        shield: toggle
+
+    switch (subcommand) {
+      case 'on': {
+        const reason = interaction.options.getString('reason')
+        const settings = await prisma.settings.findUnique({
+          where: { guild: interaction.guild.id }
+        })
+
+        if (settings.shield) {
+          return interaction.reply({ content: 'Shield is already enabled.', ephemeral: true })
+        }
+
+        await prisma.settings.upsert({
+          where: { guild: interaction.guild.id },
+          update: { shield: true },
+          create: {
+            guild: interaction.guild.id,
+            shield: true
+          }
+        })
+
+        const moderationLogChannel = interaction.guild.channels.cache.get(process.env.MODERATION_LOG_CHANNEL)
+        const moderationLogEmbed = new EmbedBuilder()
+          .setAuthor({ name: '🛡️ Shield enabled' })
+          .setDescription(`**Reason:** ${reason}`)
+          .setFooter({ text: interaction.member.user.tag })
+          .setTimestamp()
+
+        await moderationLogChannel.send({ embeds: [moderationLogEmbed] })
+
+        interaction.reply({ content: 'Shield enabled. New members who join the server will be automatically kicked until the shield is disabled.', ephemeral: true })
+        await prisma.$disconnect()
+        break
       }
-    })
 
-    await prisma.$disconnect()
+      case 'off': {
+        const reason = interaction.options.getString('reason')
+        const settings = await prisma.settings.findUnique({
+          where: { guild: interaction.guild.id }
+        })
 
-    const moderationLogChannel = interaction.guild.channels.cache.get(process.env.MODERATION_LOG_CHANNEL)
-    const moderationLogEmbed = new EmbedBuilder()
-      .setAuthor({ name: `🛡️ Shield ${toggle ? 'enabled' : 'disabled'}` })
-      .setDescription(`**Reason:** ${reason}`)
-      .setFooter({ text: interaction.member.user.tag })
-      .setTimestamp()
+        if (!settings.shield) {
+          return interaction.reply({ content: 'Shield is already disabled.', ephemeral: true })
+        }
 
-    await moderationLogChannel.send({ embeds: [moderationLogEmbed] })
+        await prisma.settings.upsert({
+          where: { guild: interaction.guild.id },
+          update: { shield: false },
+          create: {
+            guild: interaction.guild.id,
+            shield: false
+          }
+        })
 
-    return interaction.reply({ content: state[toggle], ephemeral: true })
+        const moderationLogChannel = interaction.guild.channels.cache.get(process.env.MODERATION_LOG_CHANNEL)
+        const moderationLogEmbed = new EmbedBuilder()
+          .setAuthor({ name: '🛡️ Shield disabled' })
+          .setDescription(`**Reason:** ${reason}`)
+          .setFooter({ text: interaction.member.user.tag })
+          .setTimestamp()
+
+        await moderationLogChannel.send({ embeds: [moderationLogEmbed] })
+
+        interaction.reply({ content: 'Shield disabled. New members may now join the server again.', ephemeral: true })
+        await prisma.$disconnect()
+        break
+      }
+    }
   }
 }
 
