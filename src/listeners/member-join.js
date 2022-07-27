c7d0e4ae3017dd37f4277ecc4c6523693b5a730c
Nick Pfisterer <nick@polymoon.net>
2022-07-26 18:38:51 -0700

    Detect new members with accounts created in the past 24 hours

--- src/listeners/member-join.js
+++ src/listeners/member-join.js
@@ -1,4 +1,6 @@
 import { Listener } from 'hiei.js'
+import { time } from 'discord.js'
+import ms from 'ms'
 import pkg from '@prisma/client'
 const { PrismaClient } = pkg
 
@@ -13,6 +15,7 @@ class MemberJoin extends Listener {
 
   async run (member) {
     const memberLog = await this.client.channels.fetch(process.env.MEMBER_LOG_CHANNEL)
+    const newMemberAgeLimit = new Date(Date.now() - ms(process.env.NEW_MEMBER_AGE_LIMIT))
     const prisma = new PrismaClient()
     const settings = await prisma.settings.findUnique({
       where: { guild: member.guild.id }
@@ -21,6 +24,10 @@ class MemberJoin extends Listener {
     if (settings.shield) {
       await member.kick()
       return memberLog.send({ content: `:shield: Shield prevented <@${member.user.id}> from joining the community` })
+    }
+
+    if (member.joinedTimestamp > newMemberAgeLimit) {
+      return memberLog.send({ content: `:new: <@${member.user.id}> joined the community • Account created ${time(member.user.createdTimestam, 'R')}` })
     }
 
     return memberLog.send({ content: `:green_circle: <@${member.user.id}> joined the community` })
