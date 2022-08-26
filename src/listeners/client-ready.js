bb5798878d8b7aa7a77fd89d465981863931b0b2
Nick Pfisterer <nick@polymoon.net>
2022-08-25 21:12:24 -0700

    Notify moderators of possible raid

--- src/listeners/client-ready.js
+++ src/listeners/client-ready.js
@@ -1,6 +1,6 @@
 import { Listener } from 'hiei.js'
 import Cron from 'croner'
-import { EmbedBuilder } from 'discord.js'
+import { EmbedBuilder, roleMention } from 'discord.js'
 import pkg from '@prisma/client'
 const { PrismaClient } = pkg
 
@@ -19,6 +19,7 @@ class ClientReady extends Listener {
 
     const guild = await this.client.guilds.fetch(process.env.GUILD)
     const prisma = new PrismaClient()
+    let memberCount = guild.memberCount
 
     // Remove expired strikes
     Cron('0 * * * *', async () => {
@@ -74,6 +75,19 @@ class ClientReady extends Listener {
 
         prisma.$disconnect()
       }
+    })
+
+    // Monitor for unusually high number of member joins
+    Cron('* * * * *', async () => {
+      const currentMemberCount = this.client.guilds.cache.get(process.env.GUILD).memberCount
+      const difference = currentMemberCount - memberCount
+
+      if (difference >= process.env.SUSPICIOUS_JOIN_COUNT) {
+        const channel = guild.channels.cache.get(process.env.ALERTS_CHANNEL)
+        channel.send({ content: `:warning: ${roleMention(process.env.MODERATOR_ROLE)} ${difference} new members joined the server in the last 1 minute.` })
+      }
+
+      memberCount = currentMemberCount
     })
   }
 }
