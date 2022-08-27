8d09390554fd22bdd0fe0f88bdf4395ca79438ab
Nick Pfisterer <nick@polymoon.net>
2022-08-26 17:47:42 -0700

    Fix new account detection

--- src/listeners/member-join.js
+++ src/listeners/member-join.js
@@ -1,8 +1,6 @@
 import { Listener } from 'hiei.js'
 import { time } from 'discord.js'
 import ms from 'ms'
-import pkg from '@prisma/client'
-const { PrismaClient } = pkg
 
 class MemberJoin extends Listener {
   constructor () {
@@ -15,22 +13,13 @@ class MemberJoin extends Listener {
 
   async run (member) {
     const memberLog = await this.client.channels.fetch(process.env.MEMBER_LOG_CHANNEL)
-    const newMemberAgeLimit = new Date(Date.now() - ms(process.env.NEW_MEMBER_AGE_LIMIT))
-    const prisma = new PrismaClient()
-    const settings = await prisma.settings.findUnique({
-      where: { guild: member.guild.id }
-    })
-
-    if (settings.shield) {
-      await member.kick()
-      return memberLog.send({ content: `:shield: Shield prevented <@${member.user.id}> from joining the server` })
-    }
+    const newAccountThreshold = new Date(Date.now() - ms(process.env.NEW_ACCOUNT_THRESHOLD))
 
     if (member.user.bot) {
       return memberLog.send({ content: `:robot: <@${member.user.id}> was added to the server • \`BOT\`` })
     }
 
-    if (member.joinedTimestamp > newMemberAgeLimit) {
+    if (member.createdAt > newAccountThreshold) {
       return memberLog.send({ content: `:new: <@${member.user.id}> joined the server • Account created ${time(member.user.createdAt, 'R')}` })
     }
 
