4aa438adbc39c37b35cc774dc2eb186a18e32071
Nick Pfisterer <guardian.flame9641@fastmail.com>
2023-06-19 13:21:49 -0700

    Ping > Show web socket status

--- src/commands/utility/bot-ping.js
+++ src/commands/utility/bot-ping.js
@@ -15,10 +15,21 @@ class BotPing extends SlashCommand {
     const response = await interaction.reply({ content: ':ping_pong: Ping...', ephemeral: true, fetchReply: true })
     const heartbeat = this.client.ws.ping
     const latency = response.createdTimestamp - interaction.createdTimestamp
+    const status = {
+      0: 'Ready',
+      1: 'Connecting',
+      2: 'Reconnecting',
+      3: 'Idle',
+      4: 'Nearly',
+      5: 'Disconnected',
+      6: 'WaitingForGuilds',
+      7: 'Identifying',
+      8: 'Resuming'
+    }
 
     log.info({ event: 'command-used', command: this.name, channel: interaction.channel.name.name })
 
-    return interaction.editReply({ content: `:ping_pong: Ping... Pong! Roundtrip latency is \`${latency}ms\`. Heartbeat is \`${heartbeat}ms\`.` })
+    return interaction.editReply({ content: `:ping_pong: Ping... Pong! Roundtrip latency is \`${latency}ms\`. Heartbeat is \`${heartbeat}ms\`. Status is \`${status[this.client.ws.status]}\`.` })
   }
 }
 
