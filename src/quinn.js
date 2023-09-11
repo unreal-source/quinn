40aed6365c265ee19982486206d690b05d85e3e7
Nick Pfisterer <guardian.flame9641@fastmail.com>
2023-09-11 10:25:53 -0700

    Add webhook server

--- src/quinn.js
+++ src/quinn.js
@@ -2,6 +2,7 @@ import { HieiClient } from 'hiei.js'
 import { GatewayIntentBits } from 'discord.js'
 import * as Sentry from '@sentry/node'
 import * as Tracing from '@sentry/tracing' // eslint-disable-line
+import api from './api/server.js'
 
 const client = new HieiClient({
   intents: [
@@ -19,3 +20,6 @@ Sentry.init({
 })
 
 client.login(process.env.TOKEN)
+
+api.configure(client)
+api.start()
