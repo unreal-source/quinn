34c5e1eadf2a887a0d91c52baafbf20bde1ddd77
Nick Pfisterer <guardian.flame9641@fastmail.com>
2023-09-11 11:05:41 -0700

    Remove Sentry tracing module

--- src/quinn.js
+++ src/quinn.js
@@ -1,7 +1,6 @@
 import { HieiClient } from 'hiei.js'
 import { GatewayIntentBits } from 'discord.js'
 import * as Sentry from '@sentry/node'
-import * as Tracing from '@sentry/tracing' // eslint-disable-line
 import api from './api/server.js'
 
 const client = new HieiClient({
@@ -14,9 +13,8 @@ const client = new HieiClient({
 })
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [new Sentry.Integrations.Http({ tracing: true })],
-  tracesSampleRate: 1.0
+  environment: process.env.SENTRY_ENVIRONMENT,
+  dsn: process.env.SENTRY_DSN
 })
 
 client.login(process.env.TOKEN)
